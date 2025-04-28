0cbdc328c446d57788af2fbecb19fda935225ac2
sabts <sabrinataraschi@gmail.com>
2025-04-28 11:57:39 +0200

    begining htlm

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//12 cartas
+//primero se tiene que mostrar la tarjeta por cierto tiempo y luego se voltean todas,
+//cuando le des click a 2 y no sean las mismas se voltean (time out)
+//si las cartas son las mismas, se quedan front-card
+//solo se puede seleccionar 2,  no puedes seleccionar 3
+
+//primero: generar el numero de tarjetas(12)
