63fb176d276f6b8e944c4116a3b51fbe8db9217d
sabts <sabrinataraschi@gmail.com>
2025-04-29 10:38:31 +0200

    12 cards generated

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,59 @@
 const cardsContainerElement = document.getElementById('cards-container');
 
-let pokemonCards = [
-    'assets/images/1.png',
-    'assets/images/2.png',
-    'assets/images/3.png',
-    'assets/images/4.png',
-    'assets/images/5.png',
-    'assets/images/6.png',
-    'assets/images/7.png',
-    'assets/images/8.png',
-    'assets/images/8.png',
-    'assets/images/9.png',
-    'assets/images/10.png',
-    'assets/images/11.png',
-    'assets/images/93.png',
-    'assets/images/93.png',
-    'assets/images/94.png',
-    'assets/images/144.png',
-    'assets/images/145.png',
-    'assets/images/146.png',
-    'assets/images/150.png',
-    'assets/images/151.png'
-];
+let pokemonCards = [];
 
 const randompokemonCards = () => {
-    let cardsIChooseYou = []
-    while(cardsIChooseYou.length < 12) { //por algun motivo no me da 12
-        const generateRandomPokemon = Math.floor(Math.random()*pokemonCards.length);
-        const randompokemon = pokemonCards[ generateRandomPokemon]
-        if(!cardsIChooseYou.includes(randompokemon)){
-            cardsIChooseYou.push(randompokemon)
+    while(pokemonCards.length < 6) {
+        const generateRandomPokemon = Math.floor(Math.random()*152);
+        const randompokemon = `assets/images/${generateRandomPokemon}.png`
+        if(!pokemonCards.includes(randompokemon)){
+            pokemonCards.push(randompokemon)
         }
     }
-    const pairPokemon = [...cardsIChooseYou, ...cardsIChooseYou];
+    const pairPokemon = [...pokemonCards, ...pokemonCards];
     pairPokemon.sort(() => Math.random() - 0.5);
      return pairPokemon;
 }
 
-const cardsTurnedOver = (event, pokemon) => {
+const cardIChooseYou = (event, pokemon) => {
     const cardClicked = event.target;
-    const backCard = cardsContainerElement.children[0].children[0];
+    const card = cardsContainerElement.children[0];
+    const cardBack = cardsContainerElement.children[0].children[0];
+    const CardsFront = cardsContainerElement.children[0].nextSibling
 
-    if(cardClicked === backCard){
-        cardBack.classList.add('')
+    if(cardClicked === card){
+        card.classList.add('flip-Card')
+        cardBack.classList.remove('back-card')
     }
 }
 
-const CardsFront = (randompokemonCards) => {
+const CardsFront = () => {
     cardsContainerElement.textContent = ""; //padre
     const fragment = document.createDocumentFragment(); 
-
-   pokemonCards.forEach((cardImage, iD) => {
+    const pokemon = randompokemonCards()
+    ;
+    pokemon.forEach((cardImage) => {
         const cardContainer = document.createElement("div");
         cardContainer.classList.add("card");
-        cardContainer.dataset.id = iD;
+
+        cardContainer.addEventListener("click", (event) => {
+            cardIChooseYou(event, cardContainer);
+        });
 
         //(back)
         const backCard = document.createElement("div");
         backCard.classList.add("card","back-card");
-        backCard.textContent = "?";
-       // backCard.addEventListener("click", () => {cardsTurnedOver(event,card)});
+        //backCard.textContent = "?";
 
         // (front)
         const frontCard = document.createElement("div");
         frontCard.classList.add("card","front-card");
-        frontCard.style.backgroundImage = `url(${cardImage})`;
+        frontCard.style.setProperty = (`--pokemon-picture, url(${cardImage})`);
 
         cardContainer.append(backCard, frontCard);
-        cardsContainerElement.appendChild(cardContainer);
+        fragment.append(cardContainer);
     });
-
-    //fragment.append(cardsContainerElement)
+    cardsContainerElement.append(fragment)
 };
 
 console.log(pokemonCards);
